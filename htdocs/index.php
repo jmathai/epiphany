6d5a66f50c0eca2fb91f9e11d417c23a9d74070d
Jaisen Mathai <jmathai@joythrown-lm.corp.yahoo.com>
2008-02-27 11:31:07 -0800

    Added docs.

--- htdocs/index.php
+++ htdocs/index.php
@@ -8,13 +8,9 @@
    * It is highly recommended to define a default route of '' for the home page or root of the site (yoursite.com/).
    */
   $_['routes'] = array(
-                  ''                => array(':function:', 'Home'), // yoursite.com
-                  'sample'          => array(':function:', 'MyFunction'), // yoursite.com/sample
-                  'sample/method'   => array('MyClass', 'MyMethod'), // yoursite.com/sample/method
-                  'json'            => array(':json:', array(1,2,3,4,5,6,7,8,9)), // yoursite.com/json
-                  'function/error'  => array(':function:', 'UndefinedFunction'), // yoursite.com/function/error
-                  'source'          => array(':function:', 'ViewSource'), // yoursite.com/source
-                  'sample/redirect' => array(':redirect:', 'http://www.google.com') // yoursite.com/sample/redirect
+                  ''                => array('MyClass', 'MyMethod'), // yoursite.com
+                  'sample'          => array('MyClass', 'MyOtherMethod'), // yoursite.com/sample
+                  'anypath/source'  => array('MyClass', 'ViewSource'), // yoursite.com/sample
                 );
   include_once '../php/EpiCode.php';
   
@@ -26,34 +22,37 @@
    * Define functions and classes which are executed by EpiCode based on the $_['routes'] array
    * ******************************************************************************************
    */
-  function Home()
-  {
-    echo '<h1>Welcome to the home page.  Check out these other pages.</h1>
-          <ul>
-            <li><a href="/sample">Call a function</a></li>
-            <li><a href="/sample/method">Call a method</a></li>
-            <li><a href="/json">Output some JSON</a></li>
-            <li><a href="/function/error">Call a function that does not exist</a></li>
-            <li><a href="/source">View the source of this page</a></li>
-            <li><a href="/sample/redirect">Redirect to Google</a></li>
-          </ul>';
-  }
-
-  function MyFunction()
-  {
-    echo '<h1>You called a page which executed MyFunction()</h1>';
-  }
-
-  function ViewSource()
-  {
-    highlight_file(__FILE__);
-  }
-
   class MyClass
   {
     static public function MyMethod()
     {
-      echo '<h1>You called MyClass::MyMethod</h1>';
+      echo '<h1>You are looking at the output from MyClass::MyMethod</h1>
+            <ul>
+              <li><a href="/">Call MyClass::MyMethod</a></li>
+              <li><a href="/sample">Call MyClass::MyOtherMethod</a></li>
+              <li><a href="/anypath/source">View the source of this page</a></li>
+            </ul>';
+    }
+
+    static public function MyOtherMethod()
+    {
+      echo '<h1>You are looking at the output from MyClass::MyOtherMethod</h1>
+            <ul>
+              <li><a href="/">Call MyClass::MyMethod</a></li>
+              <li><a href="/sample">Call MyClass::MyOtherMethod</a></li>
+              <li><a href="/anypath/source">View the source of this page</a></li>
+            </ul>';
+    }
+
+    static public function ViewSource()
+    {
+      echo '<h1>You are looking at the output from MyClass::ViewSource</h1>
+            <ul>
+              <li><a href="/">Call MyClass::MyMethod</a></li>
+              <li><a href="/sample">Call MyClass::MyOtherMethod</a></li>
+              <li><a href="/anypath/source">View the source of this page</a></li>
+            </ul>';
+      highlight_file(__FILE__);
     }
   }
 ?>
--- php/EpiCode.php
+++ php/EpiCode.php
@@ -1,16 +1,35 @@
 <?php
-/*
- * Class:        EpiCode
- * Description:  Minimalistic framework for PHP
- * Authors:      Jaisen Mathai <jaisen@jmathai.com>
- *               John Mcfarlane <john.mcfarlane@rockfloat.com>
+/**
+ * EpiCode master file
+ *
+ * This contains the EpiCode class as wel as the EpiError abstract class
+ * @author  Jaisen Mathai <jaisen@jmathai.com>
+ * @version 1.0  
+ * @package EpiCode
  */
 
-// Require the error handling class
+/**
+ * Require included EpiError class which extends EpiAbstract
+ */
 require_once dirname(__FILE__) . '/EpiError.php';
 
+/**
+ * This is the main EpiCode class.
+ * @name    EpiCode
+ * @author  Jaisen Mathai <jaisen@jmathai.com>
+ * @final
+ */
 final class EpiCode
 {
+  /**
+   * EpiCode::display('/path/to/template.php', $array);
+   * @name  display
+   * @author  Jaisen Mathai <jaisen@jmathai.com>
+   * @param string $template
+   * @param array $vars
+   * @method display
+   * @static method
+   */
   public static function display($template = null, $vars = null)
   {
     try
@@ -36,6 +55,15 @@ final class EpiCode
     }
   }
   
+  /**
+   * EpiCode::getRoute($_GET['__route__'], $_['routes']); 
+   * @name  getRoute
+   * @author  Jaisen Mathai <jaisen@jmathai.com>
+   * @param string $route
+   * @param array $routes
+   * @method getRoute
+   * @static method
+   */
   public static function getRoute($route = null, $routes = null)
   { 
     if($route != '.' && is_array($routes))
@@ -140,6 +168,16 @@ final class EpiCode
     }
   }
   
+  /**
+   * EpiCode::insert('/path/to/template.php'); 
+   * This method is experimental and undocumented
+   * @name  insert
+   * @author  Jaisen Mathai <jaisen@jmathai.com>
+   * @param string $template
+   * @method insert
+   * @static method
+   * @ignore
+   */
   public static function insert($template = null)
   {
     try
@@ -159,6 +197,15 @@ final class EpiCode
     }
   }
   
+  /**
+   * EpiCode::json($variable); 
+   * @name  json
+   * @author  Jaisen Mathai <jaisen@jmathai.com>
+   * @param mixed $data
+   * @return string
+   * @method json
+   * @static method
+   */
   public static function json($data)
   {
     try
@@ -178,14 +225,30 @@ final class EpiCode
     }
   }
   
+  /**
+   * EpiCode::jsonResponse($variable); 
+   * This method echo's JSON data in the header and to the screen and returns.
+   * @name  jsonResponse
+   * @author  Jaisen Mathai <jaisen@jmathai.com>
+   * @param mixed $data
+   * @method jsonResponse
+   * @static method
+   */
   public static function jsonResponse($data)
   {
     header('X-JSON: (' . json_encode($data) . ')');
     header('Content-type: application/x-json');
     echo self::json($data);
-    die();
   }
   
+  /**
+   * EpiCode::redirect($url); 
+   * @name  redirect
+   * @author  Jaisen Mathai <jaisen@jmathai.com>
+   * @param string $url
+   * @method redirect
+   * @static method
+   */
   public static function redirect($url = null)
   {
     try
@@ -206,6 +269,16 @@ final class EpiCode
     }
   }
   
+  /**
+   * EpiCode::write($contents); 
+   * This method is experimental and undocumented.
+   * @name  write
+   * @author  Jaisen Mathai <jaisen@jmathai.com>
+   * @param string $content
+   * @method write
+   * @static method
+   * @ignore
+   */
   public static function write($contents = null)
   {
     echo $contents;
@@ -213,22 +286,68 @@ final class EpiCode
   }
 }
 
+/**
+ * This is an abstract class which is extended in EpiError.php
+ * @name EpiAbstract
+ * @author Jaisen Mathai <jaisen@jmathai.com>
+ * @uses Exception
+ * @abstract class
+ */
 abstract class EpiAbstract extends Exception
 {
+  /**
+   * @usedby EpiCode::getRoute
+   */
   const EPI_ERROR_ROUTE     = 1;
+  
+  /**
+   * @usedby EpiCode::display
+   */
   const EPI_ERROR_TEMPLATE  = 2;
+
+  /**
+   * @usedby EpiCode::getRoute
+   */
   const EPI_ERROR_METHOD    = 3;
+
+  /**
+   * @usedby EpiCode::getRoute
+   */
   const EPI_ERROR_FUNCTION  = 4;
+
+  /**
+   * @usedby EpiCode::getRoute
+   */
   const EPI_ERROR_FILE      = 5;
+
+  /**
+   * @usedby EpiCode::getRoute
+   * @ignore
+   */
   const EPI_ERROR_INSERT    = 6;
+
+  /**
+   * @usedby EpiCode::json
+   * @usedby EpiCode::jsonRequest
+   */
   const EPI_ERROR_JSON      = 7;
+
+  /**
+   * @usedby EpiCode::redirect
+   */
   const EPI_ERROR_REDIRECT  = 8;
   
+  /**
+   * @ignore
+   */
   public function __construct($code, $message = '')
   {
     parent::__construct($message, $code);
   }
 
+  /**
+   * @ignore
+   */
   public function handler()
   {
     switch($this->getCode())
@@ -262,15 +381,60 @@ abstract class EpiAbstract extends Exception
     }
   }
   
-  /*
-   * Abstract methods to be defined in EpiError class
+  /**
+   * Implement this method by defining an EpiError class in EpiError.php.
+   * @name _route
+   * @author  Jaisen Mathai <jaisen@jmathai.com>
+   * @method _route
    */
   abstract protected function _route();
+
+  /**
+   * Implement this method in EpiError.
+   * @name _template
+   * @author  Jaisen Mathai <jaisen@jmathai.com>
+   * @method _template
+   */
   abstract protected function _template();
+
+  /**
+   * Implement this method in EpiError.
+   * @name _method
+   * @author  Jaisen Mathai <jaisen@jmathai.com>
+   * @method _method
+   */
   abstract protected function _method();
+
+  /**
+   * Implement this method in EpiError.
+   * @name _function
+   * @author  Jaisen Mathai <jaisen@jmathai.com>
+   * @method _function
+   */
   abstract protected function _function();
+
+  /**
+   * Implement this method in EpiError.
+   * @name _file
+   * @author  Jaisen Mathai <jaisen@jmathai.com>
+   * @method _file
+   */
   abstract protected function _file();
+
+  /**
+   * Implement this method in EpiError.
+   * @name _json
+   * @author  Jaisen Mathai <jaisen@jmathai.com>
+   * @method _json
+   */
   abstract protected function _json();
+
+  /**
+   * Implement this method in EpiError.
+   * @name _redirect
+   * @author  Jaisen Mathai <jaisen@jmathai.com>
+   * @method _redirect
+   */
   abstract protected function _redirect();
 }
 ?>
