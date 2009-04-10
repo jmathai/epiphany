d72a7a73953f9aeb1a6538de65d69c7df8650e4c
Arik Fraimovich <arik@arikfr.com>
2009-04-10 14:08:48 +0300

    added support for json array response

--- php/EpiTwitter.php
+++ php/EpiTwitter.php
@@ -15,7 +15,7 @@ class EpiTwitter extends EpiOAuth
     if(!empty($params))
       $args = array_shift($params);
 
-    return new EpiTwitterJson(call_user_func(array($this, 'httpRequest'), $method, $url, $args));
+    return EpiTwitterJson::create (call_user_func(array($this, 'httpRequest'), $method, $url, $args));
   }
 
   public function __construct($consumerKey = null, $consumerSecret = null, $oauthToken = null, $oauthTokenSecret = null)
@@ -30,22 +30,80 @@ class EpiTwitter extends EpiOAuth
 
 class EpiTwitterJson
 {
-  private $resp;
-
-  public function __construct($resp)
+  protected $httpResponse;
+	public $jsonResponse;
+	public $response;
+	
+  public function __construct($httpResponse, $jsonResponse, $response)
   {
-    $this->resp = $resp;
+    $this->httpResponse = $httpResponse;
+		$this->jsonResponse = $jsonResponse;
+		$this->response = $response;
   }
+
+	public function isValid ()
+	{
+		if($this->httpResponse->code < 200 || $this->httpResponse->code > 299)
+      return false;
+	}
+
+	public static function create ($httpResponse) 
+	{
+		$response = json_decode ($httpResponse->data, false);
+		if (is_array($response))
+		{
+			return new EpiTwitterJsonArray ($httpResponse, $httpResponse->data, $response);
+		}
+		else 
+		{
+			return new EpiTwitterJson ($httpResponse, $httpResponse->data, $response);
+		}
+	}
 
   public function __get($name)
   {
-    $this->responseText = $this->resp->data;
-    $this->response = json_decode($this->responseText, 1);
-    foreach($this->response as $k => $v)
-    {
-      $this->$k = $v;
-    }
-
-    return $this->$name;
+		if (isset($this->response->$name))
+		{
+			return $this->response->$name;
+		}
+		return false;
   }
+}
+
+class EpiTwitterJsonArray extends EpiTwitterJson implements ArrayAccess, Countable,  IteratorAggregate
+{
+	private $repsonse = array();
+
+	// Implementation of the IteratorAggregate::getIterator() to support foreach ($this as $...)
+	public function getIterator ()
+	{
+		return new ArrayIterator($this->response);
+	}
+
+	// Implementation of Countable::count() to support count($this)
+	public function count ()
+	{
+		return count($this->response);
+	}
+	
+	// Next four functions are to support ArrayAccess interface
+	public function offsetSet($offset, $value) 
+	{
+		$this->response[$offset] = $value;
+	}
+	
+	public function offsetExists($offset) 
+	{
+		return isset($this->response[$offset]);
+	}
+	
+	public function offsetUnset($offset) 
+	{
+		unset($this->response[$offset]);
+	}
+	
+	public function offsetGet($offset) 
+	{
+		return isset($this->response[$offset]) ? $this->response[$offset] : null;
+	}
 }
