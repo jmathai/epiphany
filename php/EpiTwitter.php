af19efacc6145f1730b6b8c566341115d40e695d
Jaisen Mathai <jaisen@jmathai.com>
2009-04-13 23:28:19 -0700

    Adding asynchronous curl capabilities back in after Arik's fix for JSON array responses.

--- php/EpiTwitter.php
+++ php/EpiTwitter.php
@@ -1,6 +1,7 @@
 <?php
 class EpiTwitter extends EpiOAuth
 {
+  const EPITWITTER_SIGNATURE_METHOD = 'HMAC-SHA1';
   protected $requestTokenUrl = 'http://twitter.com/oauth/request_token';
   protected $accessTokenUrl = 'http://twitter.com/oauth/access_token';
   protected $authorizeUrl = 'http://twitter.com/oauth/authorize';
@@ -15,95 +16,76 @@ class EpiTwitter extends EpiOAuth
     if(!empty($params))
       $args = array_shift($params);
 
-    return EpiTwitterJson::create (call_user_func(array($this, 'httpRequest'), $method, $url, $args));
+    return new EpiTwitterJson(call_user_func(array($this, 'httpRequest'), $method, $url, $args));
   }
 
   public function __construct($consumerKey = null, $consumerSecret = null, $oauthToken = null, $oauthTokenSecret = null)
   {
-    $this->consumerKey = $consumerKey;
-    $this->consumerSecret = $consumerSecret;
-    $this->signatureMethod = 'HMAC-SHA1';
+    parent::__construct($consumerKey, $consumerSecret, self::EPITWITTER_SIGNATURE_METHOD);
     $this->setToken($oauthToken, $oauthTokenSecret);
-    parent::__construct();
   }
 }
 
 class EpiTwitterJson
 {
-  protected $httpResponse;
-	public $jsonResponse;
-	public $response;
-	
-  public function __construct($httpResponse, $jsonResponse, $response)
+  private $__resp;
+  public function __construct($response)
   {
-    $this->httpResponse = $httpResponse;
-		$this->jsonResponse = $jsonResponse;
-		$this->response = $response;
+    $this->__resp = $response;
   }
 
-	public function isValid ()
-	{
-		if($this->httpResponse->code < 200 || $this->httpResponse->code > 299)
+  public function isValid ()
+  {
+    if($this->httpResponse->code < 200 || $this->httpResponse->code > 299)
       return false;
-	}
+  }
 
-	public static function create ($httpResponse) 
-	{
-		$response = json_decode ($httpResponse->data, false);
-		if (is_array($response))
-		{
-			return new EpiTwitterJsonArray ($httpResponse, $httpResponse->data, $response);
-		}
-		else 
-		{
-			return new EpiTwitterJson ($httpResponse, $httpResponse->data, $response);
-		}
-	}
+  // Implementation of the IteratorAggregate::getIterator() to support foreach ($this as $...)
+  public function getIterator ()
+  {
+    return new ArrayIterator($this->response);
+  }
+
+  // Implementation of Countable::count() to support count($this)
+  public function count ()
+  {
+    return count($this->response);
+  }
+  
+  // Next four functions are to support ArrayAccess interface
+  // 1
+  public function offsetSet($offset, $value) 
+  {
+    $this->response[$offset] = $value;
+  }
+
+  // 2
+  public function offsetExists($offset) 
+  {
+    return isset($this->response[$offset]);
+  }
+  
+  // 3
+  public function offsetUnset($offset) 
+  {
+    unset($this->response[$offset]);
+  }
+
+  // 4
+  public function offsetGet($offset) 
+  {
+    return isset($this->response[$offset]) ? $this->response[$offset] : null;
+  }
 
   public function __get($name)
   {
-		if (isset($this->response->$name))
-		{
-			return $this->response->$name;
-		}
-		return false;
+    $this->responseText = $this->__resp->data;
+    $this->response = json_decode($this->responseText);
+    foreach($this->response as $k => $v)
+    {
+      $this->$k = $v;
+    }
+
+    return $this->$name;
   }
-}
-
-class EpiTwitterJsonArray extends EpiTwitterJson implements ArrayAccess, Countable,  IteratorAggregate
-{
-	private $repsonse = array();
-
-	// Implementation of the IteratorAggregate::getIterator() to support foreach ($this as $...)
-	public function getIterator ()
-	{
-		return new ArrayIterator($this->response);
-	}
-
-	// Implementation of Countable::count() to support count($this)
-	public function count ()
-	{
-		return count($this->response);
-	}
-	
-	// Next four functions are to support ArrayAccess interface
-	public function offsetSet($offset, $value) 
-	{
-		$this->response[$offset] = $value;
-	}
-	
-	public function offsetExists($offset) 
-	{
-		return isset($this->response[$offset]);
-	}
-	
-	public function offsetUnset($offset) 
-	{
-		unset($this->response[$offset]);
-	}
-	
-	public function offsetGet($offset) 
-	{
-		return isset($this->response[$offset]) ? $this->response[$offset] : null;
-	}
 }
