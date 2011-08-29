84c8e39d6561f80c5921b5510f3d524f0c112b2a
Jaisen Mathai <jaisen@jmathai.com>
2011-08-29 15:48:29 -0700

    Allowing adding files using full path if starting with a . or / Adding check for config key before trying to access to avoid a PHP Notice.

--- src/EpiConfig.php
+++ src/EpiConfig.php
@@ -13,7 +13,10 @@ class EpiConfig
     $args = func_get_args();
     foreach($args as $file)
     {
-      $file = Epi::getPath('config') . "/{$file}";
+      // Prepend config directory if the path doesn't start with . or /
+      if($file[0] != '.' && $file[0] != '/')
+        $file = Epi::getPath('config') . "/{$file}";
+
       if(!file_exists($file))
       {
         EpiException::raise(new EpiConfigException("Config file ({$file}) does not exist"));
@@ -41,7 +44,7 @@ class EpiConfig
   public function get($key = null)
   {
     if(!empty($key))
-      return $this->config->$key;
+      return isset($this->config->$key) ? $this->config->$key : null;
     else
       return $this->config;
   }
