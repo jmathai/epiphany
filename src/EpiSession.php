c541565c89c497c5a8f9c13a5561101353e04746
Jaisen Mathai <jaisen@jmathai.com>
2013-03-03 20:22:40 -0800

    Fixing EpiSession::employ to honor extra parameters. Closes #61

--- src/EpiSession.php
+++ src/EpiSession.php
@@ -17,12 +17,14 @@ class EpiSession
   public static function getInstance()
   {
     $params = func_get_args();
-    $hash   = md5(implode('.', $params));
+    $hash   = md5(json_encode($params));
     if(isset(self::$instances[$hash]))
       return self::$instances[$hash];
 
-    $type = array_shift($params);
-    self::$instances[$hash] = new $type($params);
+    $type = $params[0];
+    if(!isset($params[1]))
+      $params[1] = array();
+    self::$instances[$hash] = new $type($params[1]);
     self::$instances[$hash]->hash = $hash;
     return self::$instances[$hash];
   }
@@ -34,9 +36,9 @@ class EpiSession
   public static function employ(/*$const*/)
   {
     if(func_num_args() === 1)
-    {
       self::$employ = func_get_arg(0);
-    }
+    elseif(func_num_args() > 1)
+      self::$employ = func_get_args();
 
     return self::$employ;
   }
@@ -48,17 +50,21 @@ interface EpiSessionInterface
   public function set($key = null, $value = null);
 }
 
-function getSession()
+if(!function_exists('getSession'))
 {
-  $employ = EpiSession::employ();
-  if($employ && class_exists($employ))
-    return EpiSession::getInstance($employ);
-  elseif(class_exists(EpiSession::PHP))
-    return EpiSession::getInstance(EpiSession::PHP);
-  elseif(class_exists(EpiSession::APC))
-    return EpiSession::getInstance(EpiSession::APC);
-  elseif(class_exists(EpiSession::MEMCACHED))
-    return EpiSession::getInstance(EpiSession::MEMCACHED);
-  else
-    EpiException::raise(new EpiSessionException('Could not determine which session handler to load', 404));
+  function getSession()
+  {
+    $employ = EpiSession::employ();
+    $class = array_shift($employ);
+    if($employ && class_exists($class))
+      return EpiSession::getInstance($class, $employ);
+    elseif(class_exists(EpiSession::PHP))
+      return EpiSession::getInstance(EpiSession::PHP);
+    elseif(class_exists(EpiSession::APC))
+      return EpiSession::getInstance(EpiSession::APC);
+    elseif(class_exists(EpiSession::MEMCACHED))
+      return EpiSession::getInstance(EpiSession::MEMCACHED);
+    else
+      EpiException::raise(new EpiSessionException('Could not determine which session handler to load', 404));
+  }
 }
